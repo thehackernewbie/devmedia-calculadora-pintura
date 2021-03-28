9963d9e5cf1839b062331307f552415e56310868
Hacker Newbie <63235365+thehackernewbie@users.noreply.github.com>
2021-03-28 10:02:38 -0300

    Cria classe 'Calculadora'
    Realiza o cálculo da área das paredes,
    área do teto e a litragram necessária

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+class Calculadora:
+
+    area_paredes: float
+    area_teto: float
+
+    def calcular_area_paredes(self, largura, profundidade, altura):
+        self.area_paredes = 2 * (largura + profundidade) * altura
+        return self.area_paredes
+
+    def calcular_area_teto(self, largura, profundidade):
+        self.area_teto = largura * profundidade
+        return self.area_teto
+
+    def calcular_litragem(self):
+        return (self.area_paredes + self.area_teto) / 10
