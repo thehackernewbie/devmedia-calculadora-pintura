99a2331be68d02b1a4a7cc078cf66f7f096c2b74
Hacker Newbie <63235365+thehackernewbie@users.noreply.github.com>
2021-03-28 15:56:35 -0300

    Remove as variaveis antigas
    Substitui as variaveis antigas pelos atributos
    da classe 'Comodo'

--- calculadora.py
+++ calculadora.py
@@ -3,12 +3,12 @@ class Calculadora:
     __area_paredes: float
     __area_teto: float
 
-    def calcular_area_paredes(self, largura, profundidade, altura):
-        self.__area_paredes = 2 * (largura + profundidade) * altura
+    def calcular_area_paredes(self, comodo):
+        self.__area_paredes = 2 * (comodo.largura + comodo.profundidade) * comodo.altura
         return self.__area_paredes
 
-    def calcular_area_teto(self, largura, profundidade):
-        self.__area_teto = largura * profundidade
+    def calcular_area_teto(self, comodo):
+        self.__area_teto = comodo.largura * comodo.profundidade
         return self.__area_teto
 
     def calcular_litragem(self):
