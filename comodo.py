4a8d9b1181d450c8aecfc797d2b05cf659216c49
Hacker Newbie <63235365+thehackernewbie@users.noreply.github.com>
2021-03-28 10:34:23 -0300

    Cria a classe 'Comodo'
    Recebe os dados do usuário e faz a conversão
    para o tipo float

---
+++ comodo.py
@@ -0,0 +1,9 @@
+class Comodo:
+    largura: float
+    profundidade: float
+    altura: float
+
+    def __init__(self, largura, profundidade):
+        self.largura = float(largura)
+        self.profundidade = float(profundidade)
+        self.altura = 2.9
