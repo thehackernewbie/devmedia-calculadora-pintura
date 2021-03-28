b1dd11b10d65314069f769fd244896ccf6456567
Hacker Newbie <63235365+thehackernewbie@users.noreply.github.com>
2021-03-28 15:39:53 -0300

    Priva os atributos da classe 'Calculadora'
    Os atributos apenas podem ser alterados dentro
    da classe 'Calculadora'

--- calculadora.py
+++ calculadora.py
@@ -1,15 +1,15 @@
 class Calculadora:
 
-    area_paredes: float
-    area_teto: float
+    __area_paredes: float
+    __area_teto: float
 
     def calcular_area_paredes(self, largura, profundidade, altura):
-        self.area_paredes = 2 * (largura + profundidade) * altura
-        return self.area_paredes
+        self.__area_paredes = 2 * (largura + profundidade) * altura
+        return self.__area_paredes
 
     def calcular_area_teto(self, largura, profundidade):
-        self.area_teto = largura * profundidade
-        return self.area_teto
+        self.__area_teto = largura * profundidade
+        return self.__area_teto
 
     def calcular_litragem(self):
-        return (self.area_paredes + self.area_teto) / 10
+        return (self.__area_paredes + self.__area_teto) / 10
