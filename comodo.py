eeb1784e5aac142d774b8b9d650f680ac0f266f7
Hacker Newbie <63235365+thehackernewbie@users.noreply.github.com>
2021-03-29 06:28:53 -0300

    Adiciona propriedades aos atributos privados
    As propriedades vão controlar o acesso aos atributos

--- comodo.py
+++ comodo.py
@@ -1,9 +1,21 @@
 class Comodo:
-    largura: float
-    profundidade: float
-    altura: float
+    __largura: float
+    __profundidade: float
+    __altura: float
 
     def __init__(self, largura, profundidade):
         self.largura = float(largura)
         self.profundidade = float(profundidade)
         self.altura = 2.9
+
+    @property
+    def largura(self):
+        return self.__largura
+
+    @property
+    def profundiade(self):
+        return self.__profundidade
+
+    @property
+    def altura(self):
+        return self.__altura
