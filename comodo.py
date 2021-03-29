cd28db9f767bb7ccadf02f934bf7071955a4cade
Hacker Newbie <63235365+thehackernewbie@users.noreply.github.com>
2021-03-29 07:00:25 -0300

    Cria métodos para atribuir valores
    Os métodos existentes retornam o valor dos atributos,
    estes, vão atribuir valores aos atributos e converter
    o valor para float.
    
    corrigindo pequenos erros de sintaxe

--- comodo.py
+++ comodo.py
@@ -4,18 +4,26 @@ class Comodo:
     __altura: float
 
     def __init__(self, largura, profundidade):
-        self.largura = float(largura)
-        self.profundidade = float(profundidade)
-        self.altura = 2.9
+        self.largura = largura
+        self.profundidade = profundidade
+        self.__altura = 2.9
 
     @property
     def largura(self):
         return self.__largura
 
     @property
-    def profundiade(self):
+    def profundidade(self):
         return self.__profundidade
 
     @property
     def altura(self):
         return self.__altura
+
+    @largura.setter
+    def largura(self, largura):
+        self.__largura = float(largura)
+
+    @profundidade.setter
+    def profundidade(self, profundidade):
+        self.__profundidade = float(profundidade)
